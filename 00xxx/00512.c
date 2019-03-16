e2f0d57d9f8fcaad462f641d62750d0c65f516df
Jihoon Lee <hiyabye0465@gmail.com>
2019-03-16 12:00:00 +0900

    Add 00512.c

---
+++ 00xxx/00512.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+  int value_a;
+  double value_b;
+  value_a = 49;
+  value_b = 0.268300;
+  printf("%d * %lf = %lf", value_a, value_b, value_a * value_b);
+  return 0;
+}
