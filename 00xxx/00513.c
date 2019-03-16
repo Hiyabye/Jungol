9fe31d4969a58fe1aa4127eae242d36ec992e736
Jihoon Lee <hiyabye0465@gmail.com>
2019-03-16 12:00:00 +0900

    Add 00513.c

---
+++ 00xxx/00513.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+  double yard, inch;
+  yard = 91.44;
+  inch = 2.54;
+  printf("%4.1lfyd = %5.1lfcm\n", 2.1, 2.1 * yard);
+  printf("%4.1lfin = %5.1lfcm", 10.5, 10.5 * inch);
+  return 0;
+}
