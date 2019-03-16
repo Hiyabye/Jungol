b2c7c382d5c7d33d71944cba97323ae241ad4e96
Jihoon Lee <hiyabye0465@gmail.com>
2019-03-16 12:00:00 +0900

    Add 00511.c

---
+++ 00xxx/00511.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+  int value_a;
+  int value_b;
+  value_a = 55;
+  value_b = 10;
+  printf("%d - %d = %d\n", value_a, value_b, value_a - value_b);
+  value_a = 2008;
+  value_b = 1999;
+  printf("%d - %d = %d", value_a, value_b, value_a - value_b);
+  return 0;
+}
