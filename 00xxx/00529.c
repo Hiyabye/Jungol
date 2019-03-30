38520401d88ceb267553072389215cfe8a756b07
Jihoon Lee <hiyabye0465@gmail.com>
2019-03-30 12:00:00 +0900

    Add 00529.c

---
+++ 00xxx/00529.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+  int h, w, bemahn;
+  scanf("%d %d", &h, &w);
+  bemahn = w + 100 - h;
+  printf("%d", bemahn);
+  if (bemahn > 0) {
+    printf("\nObesity");
+  }
+  return 0;
+}
