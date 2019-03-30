eb95c780a0b5038391f6c7b814fb553dd7a5e3de
Jihoon Lee <hiyabye0465@gmail.com>
2019-03-30 12:00:00 +0900

    Add 00530.c

---
+++ 00xxx/00530.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+  int age, notadult;
+  scanf("%d", &age);
+  if (age >= 20) {
+    printf("adult");
+  }
+  if (age < 20) {
+    notadult = 20 - age;
+    printf("%d years later", notadult);
+  }
+  return 0;
+}
