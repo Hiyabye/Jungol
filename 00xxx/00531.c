6d5c909b9e8808cb6acd8bcb1637c45fd0897eb2
Jihoon Lee <hiyabye0465@gmail.com>
2019-03-30 12:00:00 +0900

    Add 00531.c

---
+++ 00xxx/00531.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+  double weight;
+  scanf("%lf", &weight);
+  if (weight <= 50.80) {
+    printf("Flyweight");
+  }
+  if (50.80 < weight && weight <= 61.23) {
+    printf("Lightweight");
+  }
+  if (61.23 < weight && weight <= 72.57) {
+    printf("Middleweight");
+  }
+  if (72.57 < weight && weight <= 88.45) {
+    printf("Cruiserweight");
+  }
+  if (weight > 88.45) {
+    printf("Heavyweight");
+  }
+  return 0;
+}
