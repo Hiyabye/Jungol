1f8905ec122de46d62cf6c3d2ee250e928eb2e11
Jihoon Lee <hiyabye0465@gmail.com>
2019-04-06 12:00:00 +0900

    Add 00535.c

---
+++ 00xxx/00535.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+  float score;
+  scanf("%f", &score);
+  switch ((int)score) {
+  case 4:
+    printf("scholarship");
+    break;
+  case 3:
+    printf("next semester");
+    break;
+  case 2:
+    printf("seasonal semester");
+    break;
+  default:
+    printf("retake");
+  }
+  return 0;
+}
