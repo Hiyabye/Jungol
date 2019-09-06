8662eae584e01d7766c4d1ad900249c1f38d3dd9
Jihoon Lee <hiyabye0465@gmail.com>
2019-09-06 12:00:00 +0900

    Add 00560.c

---
+++ 00xxx/00560.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void) {
+  int i, n[10], min = 1001;
+  for (i = 0; i < 10; i++) {
+    scanf("%d", &n[i]);
+    if (n[i] < min)
+      min = n[i];
+  }
+  printf("%d", min);
+  return 0;
+}
