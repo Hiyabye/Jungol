2c2e273634a5b38283a38c09097dd6386b4951c5
Jihoon Lee <hiyabye0465@gmail.com>
2019-09-06 12:00:00 +0900

    Add 00578.c

---
+++ 00xxx/00578.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+void multiply(int n);
+
+int main(void) {
+  int a, b, save;
+  scanf("%d %d", &a, &b);
+  if (a >= b) {
+    save = a;
+    a = b;
+    b = save;
+  }
+  for (; a <= b; a++) {
+    multiply(a);
+  }
+  return 0;
+}
+
+void multiply(int n) {
+  int i;
+  printf("== %ddan ==\n", n);
+  for (i = 1; i <= 9; i++)
+    printf("%d * %d = %2d\n", n, i, n * i);
+  printf("\n");
+}
