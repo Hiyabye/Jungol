2395092fd856198b71c93d2c553539d0ea1be9f6
Jihoon Lee <hiyabye0465@gmail.com>
2019-09-06 12:00:00 +0900

    Add 00150.c

---
+++ 00xxx/00150.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void) {
+  int n;
+  char arr[10];
+  for (n = 0; n < 10; n++)
+    scanf(" %c", &arr[n]);
+  for (n = 9; n >= 0; n--)
+    printf("%c ", arr[n]);
+  return 0;
+}
