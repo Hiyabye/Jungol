2579ff1caa2da3a82efbd70e7f7eb5c01c03e1b3
Jihoon Lee <hiyabye0465@gmail.com>
2019-09-06 12:00:00 +0900

    Add 00152.c

---
+++ 00xxx/00152.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void) {
+  int i, n[10], odd = 0, even = 0;
+  for (i = 0; i < 10; i++) {
+    scanf("%d", &n[i]);
+    if (i % 2 == 0)
+      odd += n[i];
+    if (i % 2 == 1)
+      even += n[i];
+  }
+  printf("odd : %d\neven : %d", odd, even);
+  return 0;
+}
