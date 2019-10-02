c8c6e159eb64a80bc2c73b06d17025e4fcc272ca
Jihoon Lee <hiyabye0465@gmail.com>
2019-10-02 12:00:00 +0900

    Add 01303.c

---
+++ 01xxx/01303.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void) {
+  int i, j, n, m;
+  scanf("%d %d", &n, &m);
+  for (i = 0; i < n; i++) {
+    for (j = 1; j <= m; j++)
+      printf("%d ", i * m + j);
+    printf("\n");
+  }
+  return 0;
+}
