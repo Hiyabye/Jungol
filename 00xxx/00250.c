9ff993481ec3119c492a7d76eed6eca560562fad
Jihoon Lee <hiyabye0465@gmail.com>
2019-10-03 12:00:00 +0900

    Add 00250.c

---
+++ 00xxx/00250.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(void) {
+  char gender;
+  int year;
+  scanf(" %c %d", &gender, &year);
+  if (year < 2000) {
+    if (gender == 'M' || gender == 'm')
+      printf("******-1******");
+    else if (gender == 'F' || gender == 'f')
+      printf("******-2******");
+  } else if (year >= 2000) {
+    if (gender == 'M' || gender == 'm')
+      printf("******-3******");
+    else if (gender == 'F' || gender == 'f')
+      printf("******-4******");
+  }
+  return 0;
+}
