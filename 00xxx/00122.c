921a896a00aab720e9adddfcd5952c65ebb216f6
Jihoon Lee <hiyabye0465@gmail.com>
2019-10-03 12:00:00 +0900

    Add 00122.c

---
+++ 00xxx/00122.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void) {
+  int a;
+  scanf("%d", &a);
+  if (a % 4 == 0) {
+    if (a % 100 == 0) {
+      if (a % 400 == 0)
+        printf("leap year");
+      else
+        printf("common year");
+    } else
+      printf("leap year");
+  } else
+    printf("common year");
+  return 0;
+}
