111f84fd01e8881f59d9310575a34d5568728a41
Jihoon Lee <hiyabye0465@gmail.com>
2019-10-03 12:00:00 +0900

    Add 00121.c

---
+++ 00xxx/00121.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void) {
+  int a;
+  scanf("%d", &a);
+  if (a > 0)
+    printf("plus");
+  else if (a == 0)
+    printf("zero");
+  else
+    printf("minus");
+  return 0;
+}
