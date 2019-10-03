4c973d7ffa44c275f4634a54aa5290c6cd51c397
Jihoon Lee <hiyabye0465@gmail.com>
2019-10-03 12:00:00 +0900

    Add 00253.c

---
+++ 00xxx/00253.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void) {
+  int a;
+  scanf("%d", &a);
+  if (a % 4 == 0) {
+    if (a % 100 == 0) {
+      if (a % 400 == 0)
+        printf("29");
+      else
+        printf("28");
+    } else
+      printf("29");
+  } else
+    printf("28");
+  return 0;
+}
