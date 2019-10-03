60b6e9ad1ba4a2cb13c917e154bed373fc4629fb
Jihoon Lee <hiyabye0465@gmail.com>
2019-10-03 12:00:00 +0900

    Add 00251.c

---
+++ 00xxx/00251.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main(void) {
+  int a, b, c, d;
+  scanf("%d %d %d %d", &a, &b, &c, &d);
+  if (a >= b) {
+    if (a >= c) {
+      if (a >= d)
+        printf("%d", a);
+      else
+        printf("%d", d);
+    } else {
+      if (c >= d)
+        printf("%d", c);
+      else
+        printf("%d", d);
+    }
+  } else {
+    if (b >= c) {
+      if (b >= d)
+        printf("%d", b);
+      else
+        printf("%d", d);
+    } else {
+      if (c >= d)
+        printf("%d", c);
+      else
+        printf("%d", d);
+    }
+  }
+  return 0;
+}
