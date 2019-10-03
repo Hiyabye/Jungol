2103f4452cbfd36ffde0aab406d8ffca97643a35
Jihoon Lee <hiyabye0465@gmail.com>
2019-10-03 12:00:00 +0900

    Add 00581.c

---
+++ 00xxx/00581.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+double positive(double i);
+
+int main(void) {
+  double a, b;
+  scanf("%lf %lf", &a, &b);
+  if (positive(a) >= positive(b))
+    printf("%.0lf\n", a);
+  else
+    printf("%.0lf\n", b);
+  scanf("%lf %lf", &a, &b);
+  if (positive(a) <= positive(b))
+    printf("%.2lf", a);
+  else
+    printf("%.2lf", b);
+  return 0;
+}
+
+double positive(double i) {
+  if (i < 0)
+    return -i;
+  else
+    return i;
+}
