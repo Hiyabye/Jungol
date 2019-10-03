3b8e7bf02868530a55194c8e7f04d3bce4378c1e
Jihoon Lee <hiyabye0465@gmail.com>
2019-10-03 12:00:00 +0900

    Add 00249.c

---
+++ 00xxx/00249.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void) {
+  int count, price;
+  scanf("%d %d", &count, &price);
+  if (count >= 3 && count < 5)
+    printf("%.0lf", (double)price * 0.95);
+  else if (count >= 5 && count < 7)
+    printf("%.0lf", (double)price * 0.90);
+  else if (count >= 7)
+    printf("%.0lf", (double)price * 0.85);
+  else
+    printf("%d", price);
+  return 0;
+}
