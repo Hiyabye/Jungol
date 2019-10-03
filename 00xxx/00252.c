dd77af4f4c1a91b53d25adc0f39e34a279dfbf97
Jihoon Lee <hiyabye0465@gmail.com>
2019-10-03 12:00:00 +0900

    Add 00252.c

---
+++ 00xxx/00252.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(void) {
+  char a;
+  scanf(" %c", &a);
+  if (a == 'A' || a == 'a')
+    printf("Excellent");
+  else if (a == 'B' || a == 'b')
+    printf("Good");
+  else if (a == 'C' || a == 'c')
+    printf("Usually");
+  else if (a == 'D' || a == 'd')
+    printf("Effort");
+  else if (a == 'F' || a == 'f')
+    printf("Failure");
+  else
+    printf("error");
+  return 0;
+}
