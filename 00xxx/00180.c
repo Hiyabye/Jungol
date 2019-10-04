8646588dab891882e963e546f8a9685dcec7d0e9
Jihoon Lee <hiyabye0465@gmail.com>
2019-10-04 12:00:00 +0900

    Add 00180.c

---
+++ 00xxx/00180.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+void macro(int num[]);
+
+int main(void) {
+  int i, num[7];
+  for (i = 0; i < 7; i++)
+    scanf("%d", &num[i]);
+  macro(num);
+  return 0;
+}
+
+void macro(int num[]) {
+  int i, j, temp;
+  for (i = 0; i < 3; i++) {
+    for (j = 0; j < 6; j++) {
+      if (num[j] > num[j + 1]) {
+        temp = num[j];
+        num[j] = num[j + 1];
+        num[j + 1] = temp;
+      }
+    }
+  }
+  for (i = 0; i < 7; i++)
+    printf("%d ", num[i]);
+}
