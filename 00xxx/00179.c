b5ae5a3f64ea8134954a37c619bbf8da28b342f1
Jihoon Lee <hiyabye0465@gmail.com>
2019-10-04 12:00:00 +0900

    Add 00179.c

---
+++ 00xxx/00179.c
@@ -0,0 +1,30 @@
+#include <math.h>
+#include <stdio.h>
+int later(double num[]);
+int first(double num[]);
+
+int main(void) {
+  int i;
+  double num[3];
+  for (i = 0; i < 3; i++)
+    scanf("%lf", &num[i]);
+  printf("%d\n", later(num));
+  printf("%d", first(num));
+  return 0;
+}
+
+int later(double num[]) {
+  int i;
+  double sum = 0;
+  for (i = 0; i < 3; i++)
+    sum += num[i];
+  return floor(sum / 3 + 0.5);
+}
+
+int first(double num[]) {
+  int i;
+  double sum = 0;
+  for (i = 0; i < 3; i++)
+    sum += floor(num[i] + 0.5);
+  return floor(sum / 3 + 0.5);
+}
