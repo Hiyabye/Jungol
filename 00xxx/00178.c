3d4b24077c9d5b0e5896e16f4a00ebc22c70f8d3
Jihoon Lee <hiyabye0465@gmail.com>
2019-10-04 12:00:00 +0900

    Add 00178.c

---
+++ 00xxx/00178.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int calculate(int n);
+int main(void) {
+  int n;
+  scanf("%d", &n);
+  printf("%d", calculate(n));
+  return 0;
+}
+
+int calculate(int n) {
+  int i, result = 1;
+  for (i = 0; i < n; i++)
+    result *= 2;
+  return result;
+}
