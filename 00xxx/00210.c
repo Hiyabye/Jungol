75f1e031e1ab986bd61a7188f278b5c91f4ab5cd
Jihoon Lee <hiyabye0465@gmail.com>
2019-10-05 12:00:00 +0900

    Add 00210.c

---
+++ 00xxx/00210.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(void) {
+  int a;
+  scanf("%d", &a);
+  int i, j;
+  for (i = 0; i < a; i++) {
+    for (j = 0; j < a - i - 1; j++)
+      printf("  ");
+    for (j = 0; j < (i + 1) * 2 - 1; j++)
+      printf("* ");
+    printf("\n");
+  }
+  for (i = 0; i < a - 1; i++) {
+    for (j = 0; j < i + 1; j++)
+      printf("  ");
+    for (j = 0; j < (a - i - 1) * 2 - 1; j++)
+      printf("* ");
+    printf("\n");
+  }
+  return 0;
+}
