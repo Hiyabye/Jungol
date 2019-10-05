3c468c70c8c61a85bb04353f9eafda2676e012d0
Jihoon Lee <hiyabye0465@gmail.com>
2019-10-05 12:00:00 +0900

    Add 00209.c

---
+++ 00xxx/00209.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(void) {
+  int a, i = 0, count = 0, n[1000] = {'\0'};
+  while (1) {
+    scanf("%d", &n[i]);
+    if (n[i] == 0)
+      break;
+    i++;
+  }
+  for (a = 0; a < i; a++) {
+    if (n[a] % 3 == 0) {
+      if (n[a] % 5 == 0) {
+        printf("%d ", n[a]);
+        count++;
+      }
+    }
+  }
+  if (count > 0)
+    printf("\n");
+  printf("%d", count);
+  return 0;
+}
