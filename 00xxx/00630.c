e0f14221bc650f6dfef476b2060917856ff71324
Jihoon Lee <hiyabye0465@gmail.com>
2019-10-05 12:00:00 +0900

    Add 00630.c

---
+++ 00xxx/00630.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+struct string {
+  char str[100];
+};
+
+int main() {
+  int i, n;
+  struct string p[100];
+  scanf("%d", &n);
+  for (i = 0; i <= n; i++) {
+    gets(p[i].str);
+  }
+  for (i = n; i >= 0; i--) {
+    printf("%s\n", p[i].str);
+  }
+  return 0;
+}
