dcf9c122c01d1b52769c679325794c66ea78fb98
Jihoon Lee <hiyabye0465@gmail.com>
2019-10-05 12:00:00 +0900

    Add 00207.c

---
+++ 00xxx/00207.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void) {
+  int a, b;
+  char c;
+  scanf("%d %d %c", &a, &b, &c);
+  if (c == '+')
+    printf("%d %c %d = %d", a, c, b, a + b);
+  else if (c == '-')
+    printf("%d %c %d = %d", a, c, b, a - b);
+  else if (c == '*')
+    printf("%d %c %d = %d", a, c, b, a * b);
+  else if (c == '/')
+    printf("%d %c %d = %d", a, c, b, a / b);
+  else if (c == '%')
+    printf("%d %c %d = %d", a, c, b, a % b);
+  return 0;
+}
