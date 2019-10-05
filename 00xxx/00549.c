c7286ca37119f5fbe4614c93ac33a8ddbf343cdd
Jihoon Lee <hiyabye0465@gmail.com>
2019-10-05 12:00:00 +0900

    Add 00549.c

---
+++ 00xxx/00549.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void) {
+  int i, n, sum = 0;
+  scanf("%d", &n);
+  for (i = 1; i < n; i += 2) {
+    sum += i;
+    if (sum >= n)
+      break;
+  }
+  printf("%d %d", (i + 1) / 2, sum);
+  return 0;
+}
