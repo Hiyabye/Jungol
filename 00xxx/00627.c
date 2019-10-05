6334701c98e6d7b46f02db00802f654aaee49d03
Jihoon Lee <hiyabye0465@gmail.com>
2019-10-05 12:00:00 +0900

    Add 00627.c

---
+++ 00xxx/00627.c
@@ -0,0 +1,12 @@
+#include <math.h>
+#include <stdio.h>
+int main(void) {
+  int i;
+  double num[10], result;
+  for (i = 0; i < 10; i++) {
+    scanf("%lf", &num[i]);
+  }
+  result = floor((num[0] + num[9]) / 2 * 10 + 0.5);
+  printf("%.1lf", result / 10);
+  return 0;
+}
