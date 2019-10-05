ad4208016fbaee252773c420f47c27679b752e8c
Jihoon Lee <hiyabye0465@gmail.com>
2019-10-05 12:00:00 +0900

    Add 00628.c

---
+++ 00xxx/00628.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+typedef struct {
+  char name[20];
+  int score;
+  int rank;
+} student;
+
+int main(void) {
+  student p[10];
+  int i, j;
+  for (i = 0; i < 10; i++) {
+    fscanf(stdin, "%s %d", &p[i].name, &p[i].score);
+    p[i].rank = 1;
+  }
+  for (i = 0; i < 10; i++) {
+    for (j = 0; j < 10; j++) {
+      if (p[i].score < p[j].score)
+        p[i].rank++;
+    }
+  }
+  fprintf(stdout, "Name Score Rank\n");
+  for (i = 0; i < 10; i++)
+    fprintf(stdout, "%4s %5d %4d\n", p[i].name, p[i].score, p[i].rank);
+  return 0;
+}
