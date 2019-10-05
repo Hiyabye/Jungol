ef6ed31063dada2fba431417bed961a652569c09
Jihoon Lee <hiyabye0465@gmail.com>
2019-10-05 12:00:00 +0900

    Add 00211.c

---
+++ 00xxx/00211.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int year_check(int n);
+int main(void) {
+  int start, end, count = 0;
+  scanf("%d %d", &start, &end);
+  for (; start <= end; start++) {
+    if (year_check(start) == 1)
+      count++;
+  }
+  printf("%d", count);
+  return 0;
+}
+
+int year_check(int n) {
+  if (n % 4 == 0) {
+    if (n % 100 == 0) {
+      if (n % 400 == 0)
+        return 1;
+      else
+        return 0;
+    } else
+      return 1;
+  } else
+    return 0;
+}
