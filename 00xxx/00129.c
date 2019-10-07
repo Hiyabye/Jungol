752acac6e3f26ee6dfca97bffdc5c9750a014436
Jihoon Lee <hiyabye0465@gmail.com>
2019-10-07 12:00:00 +0900

    Add 00129.c

---
+++ 00xxx/00129.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(void) {
+  int a, b;
+  char choice = ' ';
+  while (1) {
+    printf("Base = ");
+    scanf("%d", &a);
+    printf("Height = ");
+    scanf("%d", &b);
+    printf("Triangle width = %.1lf\nContinue? ", (double)a * b / 2);
+    scanf(" %c", &choice);
+    if (choice == 'Y' || choice == 'y')
+      continue;
+    else
+      break;
+  }
+  return 0;
+}
