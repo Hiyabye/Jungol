c59accae564151d2275186869216fda082f8fb67
Jihoon Lee <hiyabye0465@gmail.com>
2019-10-07 12:00:00 +0900

    Add 00548.c

---
+++ 00xxx/00548.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void) {
+  int i, j;
+  for (i = 2; i <= 4; i++) {
+    for (j = 1; j <= 5; j++)
+      printf("%d * %d = %2d   ", i, j, i * j);
+    printf("\n");
+  }
+  return 0;
+}
