381d90ca9710f0e66a2fa99c4c71bc6798bcef0f
Jihoon Lee <hiyabye0465@gmail.com>
2019-10-07 12:00:00 +0900

    Add 00139.c

---
+++ 00xxx/00139.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void) {
+  int i, j, a, b;
+  scanf("%d %d", &a, &b);
+  for (i = 1; i <= 9; i++) {
+    if (a >= b) {
+      for (j = a; j >= b; j--)
+        printf("%d * %d = %2d   ", j, i, j * i);
+      printf("\n");
+    } else {
+      for (j = a; j <= b; j++)
+        printf("%d * %d = %2d   ", j, i, j * i);
+      printf("\n");
+    }
+  }
+  return 0;
+}
