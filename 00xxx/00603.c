beaa2f9e513ed22dc1cf684ba6a8214294c61d50
Jihoon Lee <hiyabye0465@gmail.com>
2019-10-07 12:00:00 +0900

    Add 00603.c

---
+++ 00xxx/00603.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+int main(void) {
+  char line[100];
+  char *token;
+  int i = 1;
+  gets(line);
+  token = strtok(line, " ");
+  while (token != NULL) {
+    if (i % 2 != 0)
+      printf("%s\n", token);
+    token = strtok(NULL, " ");
+    i++;
+  }
+  return 0;
+}
