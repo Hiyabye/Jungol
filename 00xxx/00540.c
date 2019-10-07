37aa085b623450cfaf378097d2e6eb79a4ee09ec
Jihoon Lee <hiyabye0465@gmail.com>
2019-10-07 12:00:00 +0900

    Add 00540.c

---
+++ 00xxx/00540.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void) {
+  int n;
+  scanf("%d", &n);
+  while (n != -1) {
+    if (n % 3 == 0)
+      printf("%d\n", n / 3);
+    scanf("%d", &n);
+  }
+  return 0;
+}
