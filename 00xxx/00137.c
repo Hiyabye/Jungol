a66995d12691be70caecd19a2f22701313c26b96
Jihoon Lee <hiyabye0465@gmail.com>
2019-10-07 12:00:00 +0900

    Add 00137.c

---
+++ 00xxx/00137.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void) {
+  int i, j, a, b;
+  scanf("%d %d", &a, &b);
+  for (i = 1; i <= a; i++) {
+    for (j = 1; j <= b; j++)
+      printf("%d ", j * i);
+    printf("\n");
+  }
+  return 0;
+}
