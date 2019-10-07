85bbf1ddf05dcddae788b963615a66485461ed5e
Jihoon Lee <hiyabye0465@gmail.com>
2019-10-07 12:00:00 +0900

    Add 00134.c

---
+++ 00xxx/00134.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void) {
+  int i, n, even = 0, odd = 0;
+  for (i = 0; i < 10; i++) {
+    scanf("%d", &n);
+    if (n % 2 == 0)
+      even++;
+    else
+      odd++;
+  }
+  printf("even : %d\nodd : %d", even, odd);
+  return 0;
+}
