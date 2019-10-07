8aac8b5449246795f116a64c15e5495a1a10fe1f
Jihoon Lee <hiyabye0465@gmail.com>
2019-10-07 12:00:00 +0900

    Add 00132.c

---
+++ 00xxx/00132.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void) {
+  int i, n, sum = 0;
+  scanf("%d", &n);
+  for (i = 0; i <= n; i += 5) {
+    if (i % 5 == 0)
+      sum += i;
+  }
+  printf("%d", sum);
+  return 0;
+}
