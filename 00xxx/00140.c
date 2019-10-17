f2cbdb25e209521c44b3f13dd1f9b763adf7930c
Jihoon Lee <hiyabye0465@gmail.com>
2019-10-17 12:00:00 +0900

    Add 00140.c

---
+++ 00xxx/00140.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void) {
+  int i, j, arr[20], sum = 0;
+  for (i = 0; i < 20; i++) {
+    scanf("%d", &arr[i]);
+    if (arr[i] == 0) {
+      break;
+      i--;
+    }
+  }
+  for (j = 0; j < i; j++)
+    sum += arr[j];
+  printf("%d %d", sum, sum / i);
+  return 0;
+}
