89c6bfc36d5349b16fc758427b56e70a4aa50365
Jihoon Lee <hiyabye0465@gmail.com>
2019-10-17 12:00:00 +0900

    Add 00550.c

---
+++ 00xxx/00550.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void) {
+  int i, j, n;
+  scanf("%d", &n);
+  for (i = n; i > 0; i--) {
+    for (j = 0; j < i; j++)
+      printf("*");
+    printf("\n");
+  }
+  for (i = 1; i <= n; i++) {
+    for (j = 0; j < i; j++)
+      printf("*");
+    printf("\n");
+  }
+  return 0;
+}
