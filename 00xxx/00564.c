72ce68f14ed8f12e0aadb3e38c9ac5699052a387
Jihoon Lee <hiyabye0465@gmail.com>
2019-10-23 12:00:00 +0900

    Add 00564.c

---
+++ 00xxx/00564.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void) {
+  int i, alpha[26];
+  char input;
+  scanf(" %c", &input);
+  for (i = 65; i < 91; i++)
+    alpha[i] = 0;
+  while (input >= 65 && input <= 90) {
+    alpha[input]++;
+    scanf(" %c", &input);
+  }
+  for (i = 65; i < 91; i++) {
+    if (alpha[i] > 0)
+      printf("%c : %d\n", i, alpha[i]);
+  }
+  return 0;
+}
