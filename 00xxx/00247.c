c7b5a2d3f55766be7c59f6ee39fe0b1f46072053
Jihoon Lee <hiyabye0465@gmail.com>
2019-10-23 12:00:00 +0900

    Add 00247.c

---
+++ 00xxx/00247.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void) {
+  int a, b, c;
+  scanf("%d %d %d", &a, &b, &c);
+  if (a != 0 && b != 0 && c != 0)
+    printf("1");
+  else
+    printf("0");
+  return 0;
+}
