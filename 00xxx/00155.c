f0ea295d6367588a07ada1188b55b4cf04d39551
Jihoon Lee <hiyabye0465@gmail.com>
2019-10-23 12:00:00 +0900

    Add 00155.c

---
+++ 00xxx/00155.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void) {
+  int i, success = 0;
+  char check, jungol[6] = {'J', 'U', 'N', 'G', 'O', 'L'};
+  scanf(" %c", &check);
+  for (i = 0; i < 6; i++) {
+    if (check == jungol[i]) {
+      printf("%d", i);
+      success = 1;
+      break;
+    }
+  }
+  if (success == 0)
+    printf("none");
+  return 0;
+}
