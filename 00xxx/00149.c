a86891bec97b57bae70ce8b49b00d507d0a56f97
Jihoon Lee <hiyabye0465@gmail.com>
2019-10-23 12:00:00 +0900

    Add 00149.c

---
+++ 00xxx/00149.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void) {
+  int n, i, j, num = 1;
+  scanf("%d", &n);
+  for (i = 0; i < n; i++) {
+    for (j = 0; j < n; j++) {
+      printf("%d ", num);
+      num += 2;
+      if (num > 9)
+        num %= 10;
+    }
+    printf("\n");
+  }
+}
