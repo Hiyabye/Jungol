01f0aabe8ec2e823cabbba3e58324751888c3583
Jihoon Lee <hiyabye0465@gmail.com>
2019-10-23 12:00:00 +0900

    Add 00153.c

---
+++ 00xxx/00153.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void) {
+  int i, j, num[100];
+  for (i = 0; i < 100; i++) {
+    scanf("%d", &num[i]);
+    if (num[i] == -1)
+      break;
+  }
+  if (i < 3) {
+    for (j = 0; j < i; j++)
+      printf("%d ", num[j]);
+  } else {
+    for (j = i - 3; j < i; j++)
+      printf("%d ", num[j]);
+  }
+  return 0;
+}
