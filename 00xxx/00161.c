2db611c9f9f1f75be957858f17c427535dcd350e
Jihoon Lee <hiyabye0465@gmail.com>
2019-10-24 12:00:00 +0900

    Add 00161.c

---
+++ 00xxx/00161.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void) {
+  int i, j, score[100], count[11] = {0};
+  for (i = 0; i < 100; i++) {
+    scanf("%d", &score[i]);
+    if (score[i] == 0)
+      break;
+    count[score[i] / 10]++;
+  }
+  for (i = 10; i >= 0; i--) {
+    if (count[i] > 0)
+      printf("%d : %d person\n", i * 10, count[i]);
+  }
+  return 0;
+}
