0f34b4f766740d7f869df0d21b7749e9b4b0fa54
Jihoon Lee <hiyabye0465@gmail.com>
2019-10-24 12:00:00 +0900

    Add 00160.c

---
+++ 00xxx/00160.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void) {
+  int i, n, num[6] = {0};
+  for (i = 0; i < 10; i++) {
+    scanf("%d", &n);
+    num[n - 1]++;
+  }
+  for (i = 0; i < 6; i++)
+    printf("%d : %d\n", i + 1, num[i]);
+  return 0;
+}
