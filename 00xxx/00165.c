2d3fe756025df2e4e078c11a61fe61261b775270
Jihoon Lee <hiyabye0465@gmail.com>
2019-10-24 12:00:00 +0900

    Add 00165.c

---
+++ 00xxx/00165.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(void) {
+  int i, j, arr[5][5] = {0};
+  arr[0][0] = 1, arr[0][2] = 1, arr[0][4] = 1;
+
+  for (i = 1; i < 5; i++) {
+    arr[i][0] = arr[i - 1][1];
+    for (j = 1; j < 4; j++)
+      arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j + 1];
+    arr[i][4] = arr[i - 1][3];
+  }
+
+  for (i = 0; i < 5; i++) {
+    for (j = 0; j < 5; j++)
+      printf("%d ", arr[i][j]);
+    printf("\n");
+  }
+  return 0;
+}
