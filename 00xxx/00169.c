5f44f5882cf6cc034f00e1d00a9d08c3ffbb918b
Jihoon Lee <hiyabye0465@gmail.com>
2019-10-25 12:00:00 +0900

    Add 00169.c

---
+++ 00xxx/00169.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void) {
+  int i, j;
+  char arr[3][5];
+
+  for (i = 0; i < 3; i++) {
+    for (j = 0; j < 5; j++)
+      scanf(" %c", &arr[i][j]);
+  }
+
+  for (i = 0; i < 3; i++) {
+    for (j = 0; j < 5; j++)
+      printf("%c ", arr[i][j] + 32);
+    printf("\n");
+  }
+  return 0;
+}
