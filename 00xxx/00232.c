b45a72c0e6ba67465ebc05ca8c773b961c85a484
Jihoon Lee <hiyabye0465@gmail.com>
2019-12-13 12:00:00 +0900

    Add 00232.c

---
+++ 00xxx/00232.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void) {
+  int i, n;
+
+  scanf("%d", &n);
+
+  if (n % 2 == 1) {
+    for (i = 1; i <= n; i += 2)
+      printf("%d ", i);
+  } else {
+    for (i = 2; i <= n; i += 2)
+      printf("%d ", i);
+  }
+  return 0;
+}
