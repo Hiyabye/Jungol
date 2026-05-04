53a8d5b093527d7ad87377ff7b96f55dcb3487e2
Jihoon Lee <hiyabye0465@gmail.com>
2026-05-04 12:00:00 +0900

    Add 09007.cpp

---
+++ 09xxx/09007.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  cout << "수식을 출력하면 계산 결과가 출력됩니다.\n";
+  cout << "10 + 5 = 15\n";
+  cout << "내 생일은 2005년 6월 17일 입니다.";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
