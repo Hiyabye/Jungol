2ad86d070844990bb76c16c1cb64c06de2b411e0
Jihoon Lee <hiyabye0465@gmail.com>
2026-05-04 17:21:40 +0900

    Add 02809.cpp

---
+++ 02xxx/02809.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+void solve(void) {
+  int n;
+  cin >> n;
+
+  set<int> s;
+  for (int i = 1; i * i <= n; i++) {
+    if (n % i == 0) {
+      s.insert(i);
+      s.insert(n / i);
+    }
+  }
+
+  for (int x : s) {
+    cout << x << " ";
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
