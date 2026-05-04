61f29dcd84d0803425dd8920b71a3dde52d10cbf
Jihoon Lee <hiyabye0465@gmail.com>
2026-05-04 21:49:27 +0900

    Add 09013.cpp

---
+++ 09xxx/09013.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  cout << "전체 7자리로 맞추고 소수 4자리까지 출력\n";
+  cout << "x =  1.2340\n";
+  cout << "y = 10.3459\n\n";
+  cout << "소수 2자리까지 출력(반올림)\n";
+  cout << "x = 1.23\n";
+  cout << "y = 10.35";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
