7578f734e88f7d5fac1a68999d7272a12b6b2a1f
Jihoon Lee <hiyabye0465@gmail.com>
2026-05-04 16:26:32 +0900

    Add 01430.cpp

---
+++ 01xxx/01430.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void solve(void) {
+  int a, b, c;
+  cin >> a >> b >> c;
+
+  string s = to_string(a * b * c);
+  for (int i = 0; i < 10; i++) {
+    int cnt = 0;
+    for (char c : s)
+      cnt += (c - '0' == i);
+    cout << cnt << "\n";
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
