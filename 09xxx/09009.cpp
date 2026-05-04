642f325cc3a4635fa77a2227c05e28d8ecb2afe8
Jihoon Lee <hiyabye0465@gmail.com>
2026-05-04 16:13:13 +0900

    Add 09009.cpp

---
+++ 09xxx/09009.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  cout << "a = 10\n";
+  cout << "b = A";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
