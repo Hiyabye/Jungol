263629fce4ef43f6ce35fdc3c0554b76f5f084db
Jihoon Lee <hiyabye0465@gmail.com>
2026-05-04 17:40:37 +0900

    Add 04637.cpp

---
+++ 04xxx/04637.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+void solve(void) {
+  int q;
+  cin >> q;
+
+  set<int> s;
+  while (q--) {
+    char op;
+    cin >> op;
+    if (op == 'i') {
+      int x;
+      cin >> x;
+      s.insert(x);
+    } else if (op == 'r') {
+      int x;
+      cin >> x;
+      s.erase(x);
+    }
+  }
+
+  for (int x : s) {
+    cout << x << " ";
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
