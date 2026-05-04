9f1e5b9ffae7652db700eefe43fd0b380aca265c
Jihoon Lee <hiyabye0465@gmail.com>
2026-05-04 17:20:28 +0900

    Add 01402.cpp

---
+++ 01xxx/01402.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+void solve(void) {
+  int n, k;
+  cin >> n >> k;
+
+  set<int> s;
+  for (int i = 1; i * i <= n; i++) {
+    if (n % i == 0) {
+      s.insert(i);
+      s.insert(n / i);
+    }
+  }
+
+  if (s.size() < k) {
+    cout << 0;
+  } else {
+    auto it = s.begin();
+    while (--k) {
+      it++;
+    }
+    cout << *it;
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
