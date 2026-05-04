82d4d3667092face31f5732269a5c4d056bdd85d
Jihoon Lee <hiyabye0465@gmail.com>
2026-05-04 16:08:54 +0900

    Add 09008.cpp

---
+++ 09xxx/09008.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  cout << " subject score\n";
+  cout << "==============\n";
+  cout << "  korean    90\n";
+  cout << " english   100\n";
+  cout << "computer    80";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
