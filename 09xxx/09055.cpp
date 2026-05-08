3517c56716e9b12b270713559a12405d7a61de50
Jihoon Lee <hiyabye0465@gmail.com>
2026-05-08 10:24:25 +0900

    Add 09055.cpp

---
+++ 09xxx/09055.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int n;
+  cin >> n;
+
+  int ans = 1;
+  while (ans * (ans + 1) / 2 <= n) {
+    ans++;
+  }
+  cout << ans << " " << ans * (ans + 1) / 2;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
