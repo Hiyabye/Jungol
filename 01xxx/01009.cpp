4a952b34568ebb7141dbcb944d88e28538a07176
Jihoon Lee <hiyabye0465@gmail.com>
2026-05-10 10:23:20 +0900

    Add 01009.cpp

---
+++ 01xxx/01009.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void solve(void) {
+  string n;
+  cin >> n;
+  while (n != "0") {
+    while (n.back() == '0') {
+      n.pop_back();
+    }
+    for (int i = 0; i < n.size(); i++) {
+      cout << n[n.length() - i - 1];
+    }
+    cout << " ";
+    int sum = 0;
+    for (char c : n) {
+      sum += c - '0';
+    }
+    cout << sum << "\n";
+    cin >> n;
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
