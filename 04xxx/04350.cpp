06d979e84c6e6eed99200684260a6a9e9740e6e1
Jihoon Lee <hiyabye0465@gmail.com>
2026-05-10 13:09:41 +0900

    Add 04350.cpp

---
+++ 04xxx/04350.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  int n, k;
+  cin >> n >> k;
+  vector<int> v(n);
+  for (int i = 0; i < n; i++) {
+    string s;
+    cin >> s;
+    v[i] = s.length();
+  }
+
+  long long ans = 0;
+  vector<int> cnt(21, 0);
+  for (int i = 0; i < k; i++) {
+    ans += cnt[v[i]]++;
+  }
+  for (int i = k; i < n; i++) {
+    ans += cnt[v[i]]++;
+    cnt[v[i - k]]--;
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
