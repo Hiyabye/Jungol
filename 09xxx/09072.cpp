01f4459789de61312b2f5457d8a2ba20230d0a42
Jihoon Lee <hiyabye0465@gmail.com>
2026-05-12 10:24:27 +0900

    Add 09072.cpp

---
+++ 09xxx/09072.cpp
@@ -0,0 +1,25 @@
+#include <iomanip>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  vector<int> v(10);
+  for (int i = 0; i < 10; i++)
+    cin >> v[i];
+
+  int sum = 0;
+  for (int i = 0; i < 10; i++)
+    sum += v[i];
+
+  cout << "총점 = " << sum << "\n";
+  cout << "평균 = " << fixed << setprecision(1) << (double)sum / 10;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
