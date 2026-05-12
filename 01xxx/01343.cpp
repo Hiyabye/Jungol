e97e30c93afb76ee7e433dc5c83acf1ce9496fb1
Jihoon Lee <hiyabye0465@gmail.com>
2026-05-12 20:51:33 +0900

    Add 01343.cpp

---
+++ 01xxx/01343.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int a, b, c;
+  cin >> a >> b >> c;
+
+  cout << c * 10000 / b / a;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
