0a317b0d36e5763dea2a04799869f639eaaa18cc
Jihoon Lee <hiyabye0465@gmail.com>
2026-05-14 00:33:19 +0900

    Add 01438.cpp

---
+++ 01xxx/01438.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  int n;
+  cin >> n;
+
+  vector<vector<bool>> v(101, vector<bool>(101, false));
+  while (n--) {
+    int x, y;
+    cin >> x >> y;
+    for (int i = x; i < x + 10; i++) {
+      for (int j = y; j < y + 10; j++) {
+        v[i][j] = true;
+      }
+    }
+  }
+
+  int ans = 0;
+  for (int i = 0; i <= 100; i++) {
+    for (int j = 0; j <= 100; j++) {
+      if (v[i][j])
+        ans++;
+    }
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
