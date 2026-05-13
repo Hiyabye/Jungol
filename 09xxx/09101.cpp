d9335bf0564547279c0d601a3e4ad18f7ec5271f
Jihoon Lee <hiyabye0465@gmail.com>
2026-05-13 10:46:14 +0900

    Add 09101.cpp

---
+++ 09xxx/09101.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  cout << "홍길동\n";
+  cout << "홍길동\n";
+  cout << "홍길동\n";
+  cout << "홍길동\n";
+  cout << "홍길동\n";
+  cout << "홍길동\n";
+  cout << "홍길동\n";
+  cout << "홍길동\n";
+  cout << "홍길동\n";
+  cout << "홍길동";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
