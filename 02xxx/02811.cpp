faa8f140c479729c9899a8c5485876416c79e21b
Jihoon Lee <hiyabye0465@gmail.com>
2026-05-16 20:18:56 +0900

    Add 02811.cpp

---
+++ 02xxx/02811.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+inline bool prime(int n) {
+  if (n < 2)
+    return false;
+  for (int i = 2; i * i <= n; i++)
+    if (n % i == 0)
+      return false;
+  return true;
+}
+
+void solve(void) {
+  int n;
+  cin >> n;
+
+  if (n == 1) {
+    cout << "number one\n";
+  } else if (prime(n)) {
+    cout << "prime number\n";
+  } else {
+    cout << "composite number\n";
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t = 5;
+  while (t--)
+    solve();
+  return 0;
+}
