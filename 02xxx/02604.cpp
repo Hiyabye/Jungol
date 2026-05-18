34278a7c9b913a7c1dd23d7afd0286e4b97e9ed6
Jihoon Lee <hiyabye0465@gmail.com>
2026-05-18 10:58:24 +0900

    Add 02604.cpp

---
+++ 02xxx/02604.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void solve(void) {
+  string s;
+  cin >> s;
+
+  int ans = 10;
+  for (int i = 1; i < s.length(); i++) {
+    ans += (s[i] != s[i - 1]) ? 10 : 5;
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
