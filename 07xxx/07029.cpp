03faec83667ccd1b69b57dfa2f62371b1025fa62
Jihoon Lee <hiyabye0465@gmail.com>
2026-05-20 10:58:33 +0900

    Add 07029.cpp

---
+++ 07xxx/07029.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) { cout << "(^^) (--) (++) (xx)"; }
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
