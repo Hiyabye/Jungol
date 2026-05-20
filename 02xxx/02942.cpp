0f310bb4596756a8623687f3d605391d970899b4
Jihoon Lee <hiyabye0465@gmail.com>
2026-05-20 11:02:23 +0900

    Add 02942.cpp

---
+++ 02xxx/02942.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  int n;
+  cin >> n;
+
+  vector<long long> dp = {0, 1, 1, 1, 2, 2};
+  for (int i = 6; i <= 100; i++) {
+    dp[i] = dp[i - 1] + dp[i - 5];
+  }
+  cout << dp[n];
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
