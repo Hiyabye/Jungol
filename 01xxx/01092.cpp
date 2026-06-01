6ce07fe14c08e6c78782fb12e1ad93a2d815746d
Jihoon Lee <hiyabye0465@gmail.com>
2026-06-02 08:47:07 +0900

    Add 01092.cpp

---
+++ 01xxx/01092.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#define MOD 20091024
+using namespace std;
+
+inline long long power(long long base, long long exp) {
+  long long ret = 1;
+  while (exp) {
+    if (exp & 1) {
+      ret = ret * base % MOD;
+    }
+    base = base * base % MOD;
+    exp >>= 1;
+  }
+  return ret;
+}
+
+void solve(void) {
+  int x, y;
+  cin >> x >> y;
+
+  cout << power(x, y);
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
