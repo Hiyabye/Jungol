e584788e4987db5fc3522f0407e7f271c6a6a5e4
Jihoon Lee <hiyabye0465@gmail.com>
2026-06-03 12:39:08 +0900

    Add 06161.cpp

---
+++ 06xxx/06161.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+void solve(void) {
+  int n;
+  cin >> n;
+
+  stack<int> s;
+  while (n--) {
+    int x;
+    cin >> x;
+    while (!s.empty() && s.top() >= x)
+      s.pop();
+    cout << (s.empty() ? 0 : s.top()) << " ";
+    s.push(x);
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
