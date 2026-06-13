0d3b3e91cd83f381124f076a2088dc4a06af2487
Jihoon Lee <hiyabye0465@gmail.com>
2026-06-13 19:20:14 +0900

    Add 09026.cpp

---
+++ 09xxx/09026.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int a = 0, b = 1, c = 2;
+
+  cout << (a < b) << " " << (b < c) << " " << (c < a) << " " << (!a);
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
