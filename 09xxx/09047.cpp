fb520657e3ac20d4edff93422ea9b3f05b92e48e
Jihoon Lee <hiyabye0465@gmail.com>
2026-06-13 18:55:46 +0900

    Add 09047.cpp

---
+++ 09xxx/09047.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  for (char c = 'A'; c <= 'Z'; c++) cout << c;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
