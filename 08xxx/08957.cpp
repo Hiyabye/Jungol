2fa7eece6ffb69a09a5419bd8b93a5841c4b0677
Jihoon Lee <hiyabye0465@gmail.com>
2026-06-13 20:44:50 +0900

    Add 08957.cpp

---
+++ 08xxx/08957.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int a, b;
+  cin >> a >> b;
+
+  cout << a * b;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
