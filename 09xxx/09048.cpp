362f6b58290fe78604983ec9364d45609795c9ee
Jihoon Lee <hiyabye0465@gmail.com>
2026-06-13 18:56:14 +0900

    Add 09048.cpp

---
+++ 09xxx/09048.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  for (int i = 1; i <= 20; i += 2) cout << i << " ";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
