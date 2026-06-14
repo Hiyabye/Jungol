dda6a12b2c31fed50ed361fd25f74bc685f8e557
Jihoon Lee <hiyabye0465@gmail.com>
2026-06-14 12:57:32 +0900

    Add 03522.cpp

---
+++ 03xxx/03522.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#define MOD 1000000007
+using namespace std;
+
+void solve(void) {
+  int n;
+  cin >> n;
+
+  vector<long long> f(n + 1, 0);
+  f[1] = 1;
+  for (int i = 2; i <= n; i++) f[i] = (f[i - 1] + f[i - 2]) % MOD;
+
+  cout << f[n];
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
