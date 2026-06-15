afe1fcedb32beea3fe1601b2011f2337fa32e05c
Jihoon Lee <hiyabye0465@gmail.com>
2026-06-15 12:37:17 +0900

    Add 05947.cpp

---
+++ 05xxx/05947.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int n;
+  cin >> n;
+  if (n < 1 || n > 50 || n % 2 == 0) {
+    cout << "INPUT ERROR!";
+    return;
+  }
+
+  for (int i = 0; i <= n / 2; i++) {
+    for (int j = 0; j <= i; j++) cout << j + 1 << " ";
+    cout << "\n";
+  }
+  for (int i = n / 2 - 1; i >= 0; i--) {
+    for (int j = 0; j <= i; j++) cout << j + 1 << " ";
+    cout << "\n";
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
