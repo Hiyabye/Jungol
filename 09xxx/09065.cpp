980718d804f44f9fd3fbf4de383f7cb05bf236e9
Jihoon Lee <hiyabye0465@gmail.com>
2026-06-16 10:30:04 +0900

    Add 09065.cpp

---
+++ 09xxx/09065.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  vector<int> v(5);
+  for (int i = 0; i < 5; i++) cin >> v[i];
+
+  for (int i = 0; i < 5; i++) cout << v[i] << " ";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
