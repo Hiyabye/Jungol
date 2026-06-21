3218a6e894bd191b795d3fb70a99fb8c3ca0103f
Jihoon Lee <hiyabye0465@gmail.com>
2026-06-22 00:03:52 +0900

    Add 08557.cpp

---
+++ 08xxx/08557.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <iostream>
+#include <utility>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  int n;
+  cin >> n;
+  vector<pair<int, int>> v(n);
+  for (int i = 0; i < n; i++) cin >> v[i].first >> v[i].second;
+  sort(v.begin(), v.end());
+
+  for (int i = 0; i < n; i++) cout << v[i].first * v[i].second << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
