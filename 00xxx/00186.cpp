eff19080a38ae971c042a827a48ef937df02d831
Jihoon Lee <hiyabye0465@gmail.com>
2026-07-12 16:59:31 +0900

    Add 00186.cpp

---
+++ 00xxx/00186.cpp
@@ -0,0 +1,19 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+using namespace std;
+
+void solve(void) {
+  string s, t;
+  cin >> s >> t;
+
+  cout << max(s.length(), t.length());
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
