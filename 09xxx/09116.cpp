5f09a6be30b5f293036c3691d4f9a27c25b2e839
Jihoon Lee <hiyabye0465@gmail.com>
2026-07-12 16:37:35 +0900

    Add 09116.cpp

---
+++ 09xxx/09116.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+using namespace std;
+
+void solve(void) {
+  string s;
+  getline(cin, s);
+
+  stringstream ss(s);
+  while (ss >> s) cout << s << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
