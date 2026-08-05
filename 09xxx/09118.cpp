26bc93f14768f1c0cf0356a88b1291e42b1cbeed
Jihoon Lee <hiyabye0465@gmail.com>
2026-08-06 08:34:49 +0900

    Add 09118.cpp

---
+++ 09xxx/09118.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  vector<string> v(5);
+  for (int i = 0; i < 5; i++) cin >> v[i];
+
+  for (int i = 0; i < 5; i++) cout << v[i] << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
