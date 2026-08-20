1bb92de0ec6dd740c81506053d4cc2805a5703a4
Jihoon Lee <hiyabye0465@gmail.com>
2026-08-21 02:13:38 +0900

    Add 09123.cpp

---
+++ 09xxx/09123.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void solve(void) {
+  string a, b;
+  cin >> a >> b;
+
+  cout << b << a;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
