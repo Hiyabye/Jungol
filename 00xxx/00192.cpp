7c9ada586af2fc07b70d777a11c10b2c487182ea
Jihoon Lee <hiyabye0465@gmail.com>
2026-08-27 12:00:44 +0900

    Add 00192.cpp

---
+++ 00xxx/00192.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  int n;
+  cin >> n;
+  vector<string> v(n);
+  for (int i = 0; i < n; i++) cin >> v[i];
+  sort(v.begin(), v.end());
+
+  for (int i = 0; i < n; i++) cout << v[i] << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
