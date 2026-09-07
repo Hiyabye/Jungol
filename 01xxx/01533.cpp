82f88b984f9fe6b38db716d53e4ba9d17018efaf
Jihoon Lee <hiyabye0465@gmail.com>
2026-09-07 11:00:33 +0900

    Add 01533.cpp

---
+++ 01xxx/01533.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int a, b;
+  cin >> a >> b;
+
+  cout << (a + b) / 2 << " " << (a - b) / 2;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
