86c747281e4b05a4d56251b45824c1d90319c5f9
Jihoon Lee <hiyabye0465@gmail.com>
2026-09-08 12:30:54 +0900

    Add 04962.cpp

---
+++ 04xxx/04962.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int n;
+  cin >> n;
+
+  for (int i = 1; i <= n; i++) {
+    for (int j = 0; j < 5; j++) cout << i;
+    cout << "\n";
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
