353b08510172e343fc09cdffbd5083b76a6f5840
Jihoon Lee <hiyabye0465@gmail.com>
2026-09-14 00:14:14 +0900

    Add 05844.cpp

---
+++ 05xxx/05844.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int x;
+  cin >> x;
+
+  cout << x * x * x;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
