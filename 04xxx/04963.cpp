c48fc959feaac6104b1eee6e693aed14a26d3b08
Jihoon Lee <hiyabye0465@gmail.com>
2026-09-16 00:43:35 +0900

    Add 04963.cpp

---
+++ 04xxx/04963.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int n;
+  cin >> n;
+
+  for (int i = 1; i <= n; i++) cout << i << i + 1 << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
