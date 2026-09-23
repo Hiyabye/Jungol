69e31ae94f1d361e108e63314c8f0d915b2e9c64
Jihoon Lee <hiyabye0465@gmail.com>
2026-09-23 15:40:26 +0900

    Add 05054.cpp

---
+++ 05xxx/05054.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void solve(void) {
+  string s;
+  cin >> s;
+
+  cout << s[1] << s[1] << s[1];
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
