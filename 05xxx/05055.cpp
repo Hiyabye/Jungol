9612974048f5296441a6281d37c0fdeaf55f14cc
Jihoon Lee <hiyabye0465@gmail.com>
2026-09-23 15:41:10 +0900

    Add 05055.cpp

---
+++ 05xxx/05055.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void solve(void) {
+  string s;
+  cin >> s;
+
+  cout << s.back() << s.back();
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
